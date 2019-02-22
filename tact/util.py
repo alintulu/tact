fbd9d64ee3204ba26d0efe7920e8ac66f08b13aa
lunik1 <lunik1@users.noreply.github.com>
2019-02-22 21:00:05 +0000

    Remove 2D MVA code

--- tact/util.py
+++ tact/util.py
@@ -94,34 +94,6 @@ def nodes(tree):
     return nodelist
 
 
-def s_to_n(cat, w=None):
-    """
-    Calculate the signal-to-noise ratio.
-
-    Parameters
-    ----------
-    cat : 1D array, shape=N
-        Array containing labels describing whether an entry is signal (1 or
-        True) or background (0 or False).
-    w : 1D array, shape=N, optional
-        Weights. If none, then equal weights are used.
-
-    Returns
-    -------
-    float
-        Signal-to-noise ratio
-    """
-
-    if w is None:
-        signal = len(cat[cat == 1])
-        noise = len(cat[cat == 0])
-    else:
-        signal = w[cat == 1].sum()
-        noise = w[cat == 0].sum()
-
-    return signal / noise
-
-
 def maenumerate(marr):
     """
     Multidimensional index iterator for masked arrays.
